21e778ec48f3533ce8d1c363db6347016f9dd6e8
Paulus Esterhazy <pesterhazy@gmail.com>
2016-12-04 21:18:24 +0100

    WIP

--- src/boot_fmt/core.clj
+++ src/boot_fmt/core.clj
@@ -6,10 +6,14 @@
 (bc/deftask fmt
   "Reformat Clojure(script) source files, like gofmt"
   [m mode MODE kw "Mode"
+   r really bool "Really act?"
    f files VAL #{str} "file(s) to format"]
   (let [mode (or mode :list)]
     (assert (seq files) "At least one filename needs to be provided.")
     (assert (#{:list :diff :overwrite} mode) "Invalid mode")
+    (assert (or (not= :overwrite mode)
+                really)
+            "In overwrite mode, add the --really flag")
     (bc/with-pre-wrap fileset
       (let [files* (some->> files
                             (map clojure.java.io/file)
