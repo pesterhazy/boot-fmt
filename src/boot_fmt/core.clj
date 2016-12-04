38d371387624aad5d153fa14f55ea2678483b351
Paulus Esterhazy <pesterhazy@gmail.com>
2016-12-04 19:08:36 +0100

    WIP

--- src/boot_fmt/core.clj
+++ src/boot_fmt/core.clj
@@ -5,7 +5,10 @@
 (def test-out-fname "test/files/response-out.clj")
 
 (defn transform [contents]
-  (str (zp/zprint-str contents {:parse-string-all? true :parse {:interpose "\n\n"}}) "\n"))
+  (str (zp/zprint-str contents {:parse-string-all? true
+                                :parse {:interpose "\n\n"}
+                                :style :community})
+       "\n"))
 
 (defn run []
   (->> test-fname
